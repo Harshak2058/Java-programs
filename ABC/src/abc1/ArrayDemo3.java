7306070b9c46bdeeccb3df7c5c03e100d00c6aff
Harshak2058 <harshak2058@gmail.com>
2024-07-04 15:31:29 +0530

    array3

---
+++ ABC/src/abc1/ArrayDemo3.java
@@ -0,0 +1,11 @@
+package abc1;
+
+public class ArrayDemo3 {
+
+	public static void main(String[] args) {
+		int num[]= {10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200};
+		for(int i=0;i<num.length;i++)
+			System.out.println(num[i]);
+	}
+
+}
