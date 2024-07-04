51308a1f85a58f3639f4fd8f3716c48cfc3b2df2
Harshak2058 <harshak2058@gmail.com>
2024-07-04 15:48:46 +0530

    arrayDemo4

---
+++ ABC/src/abc1/ArrayDemo4.java
@@ -0,0 +1,14 @@
+package abc1;
+
+public class ArrayDemo4 {
+
+	public static void main(String[] args) 
+   {
+int age[]= {23,54,43};
+	for(int a:age)
+	{
+System.out.println(a);
+    }
+   }
+}
+//ARRAY USING FOR-EACH LOOP
