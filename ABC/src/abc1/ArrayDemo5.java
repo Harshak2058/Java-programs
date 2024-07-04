62acae1ea6bcff4aaba0dd80fa27a97baccac7d3
Harshak2058 <harshak2058@gmail.com>
2024-07-04 15:50:21 +0530

    arrayDemo5

---
+++ ABC/src/abc1/ArrayDemo5.java
@@ -0,0 +1,12 @@
+package abc1;
+
+public class ArrayDemo5 
+{
+public static void main(String[] args) {
+	String name[]= {"qwerty","yuiop","asdfgh"};
+	for(String a:name)
+	{
+		System.out.println(a);
+	}
+}
+}
