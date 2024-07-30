32f23d1a3b7e14bdc7de622ae7839bc7bd520f9e
Harshak2058 <harshak2058@gmail.com>
2024-07-30 14:48:12 +0530

    COMMIT

---
+++ Operators/src/arthimeticOperators/ClassA.java
@@ -0,0 +1,16 @@
+package arthimeticOperators;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		int a=55;  
+		int b=3;  
+		System.out.println(a+b);//addition
+		System.out.println(a-b);  //subtraction
+		System.out.println(a*b); //multiply
+		System.out.println(a/b);  //division
+		System.out.println(a%b);  //modulus i.e remainder when b divides a
+                      //as we took int datatype it wont go for decimal numbers
+	}
+
+}
