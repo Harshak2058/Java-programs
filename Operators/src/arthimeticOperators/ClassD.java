96093fee53d25735004e455f10ac7efb2274de63
Harshak2058 <harshak2058@gmail.com>
2024-07-30 14:50:13 +0530

    commit

---
+++ Operators/src/arthimeticOperators/ClassD.java
@@ -0,0 +1,18 @@
+package arthimeticOperators;
+
+public class ClassD {
+
+	public static void main(String[] args) {
+	     int n1 = 20, n2 = 10, div = 0;
+  
+        System.out.println("num1 = "+ n1);
+
+        System.out.println("num2 = "+ n2);
+  
+      
+        div = n1 / n2;
+        System.out.println("Division = "+ div);
+
+	}
+
+}
